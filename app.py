d1f6d787278cde349836acf2e1330d3d419ee358
mia7756 <58284444+mia7756@users.noreply.github.com>
2023-03-05 11:49:11 +0800

    add function 找甲級星的輔曜佐曜

--- app.py
+++ app.py
@@ -168,3 +168,127 @@ def stars_main(day: int, round: str) -> dict:
             if star := rule[(i - start + 12) % 12]:
                 result[earthly].append(star)
     return result
+
+
+def stars_lucky(
+        birth_year_heavenaly: str, birth_year_earthly: str,
+        birth_month: str, birth_hour: str
+    ) -> dict:
+    """甲級星的輔星 八吉星
+    Ex: 出生 2023-03-01 丑時(農曆癸卯年二月初十 丑時)
+    input: "癸", "卯", "卯", "丑"
+    output: {'子': ['祿存'], '丑': [], '寅': [], '卯': ['天魁'],...}
+
+    Args:
+        birth_year_heavenaly (str): 出生年(天干)
+        birth_year_earthly (str): 出生年(地支)
+        birth_month (str): 出生月(地支)
+        birth_hour (str): 出生時(地支)
+
+    Returns:
+        dict: 子丑寅卯...對應的星曜
+    """
+    # 左輔: 以辰起始順時針 (起始值 4 + (寅月(i=2) - 2)) => 2 + 出生月(地支)
+    # 右弼: 以戌起始逆時針 (起始值 10 - (寅月(i=2) - 2)) => 12 - 出生月(地支)
+    # 文曲: 以辰起始順時針 (起始值 4 + (子時(i=0))) => 4 + 出生時(地支)
+    # 文昌: 以戌起始逆時針 (起始值 10 - (子時(i=0))) => 10 - 出生月(地支)
+    # 天魁,天鉞: 出生年(天干)
+    # 甲(0)戊(4)庚(6) ..... 牛(1),羊(7)
+    # 乙(1)己(5)      ..... 鼠(0),猴(8)
+    # 丙(2)丁(3)      ..... 豬(11),雞(9)
+    # 辛(7)           ..... 馬(6),虎(2)
+    # 壬(8)癸(9)      ..... 兔(3),蛇(5)
+    # 祿存: 出生年(天干)
+    # 甲(0)      ..... 寅(2)
+    # 乙(1)      ..... 卯(3)
+    # 丙(2)戊(4) ..... 巳(5)
+    # 丁(3)已(5) ..... 午(6)
+    # 庚(6)      ..... 巳(8)
+    # 辛(7)      ..... 午(9)
+    # 壬(8)      ..... 亥(11)
+    # 癸(9)      ..... 子(0)
+    result = {earthly: [] for earthly in EARTHLYS}
+    result[EARTHLYS[(2 + EARTHLYS.index(birth_month)) % 12]].append("左輔")
+    result[EARTHLYS[(12 - EARTHLYS.index(birth_month)) % 12]].append("右弼")
+    result[EARTHLYS[(4 + EARTHLYS.index(birth_hour)) % 12]].append("文曲")
+    result[EARTHLYS[(10 - EARTHLYS.index(birth_hour)) % 12]].append("文昌")
+    # 天魁, 天鉞, 祿存
+    rule = [
+        (1, 7, 2), (0, 8, 3), (11, 9, 5), (11, 9, 6), (1, 7, 5),
+        (0, 8, 6), (1, 7, 8), (6, 2, 9), (3, 5, 11), (3, 5, 0),
+    ]
+    star = rule[HEAVENLYS.index(birth_year_heavenaly)]
+    result[EARTHLYS[star[0]]].append("天魁")
+    result[EARTHLYS[star[1]]].append("天鉞")
+    result[EARTHLYS[star[2]]].append("祿存")
+    # 天馬
+    rule = [2, 11, 8, 5]
+    result[EARTHLYS[rule[EARTHLYS.index(birth_year_earthly) % 4]]].append("天馬")
+    return result
+
+
+def stars_unluckly(
+        birth_year_heavenaly: str, birth_year_earthly: str, birth_hour: str
+    ) -> dict:
+    """甲級星的輔星 六煞星
+    Ex: 出生 2023-03-01 丑時(農曆癸卯年二月初十 丑時)
+    input: "癸", "卯", "丑"
+    output: {'子': ['地劫'], '丑': ['擎羊'], '寅': [], '卯': [],...}
+
+    Args:
+        birth_year_heavenaly (str): 出生年(天干)
+        birth_year_earthly (str): 出生年(地支)
+        birth_hour (str): 出生時(地支)
+
+    Returns:
+        dict: 子丑寅卯...對應的星曜
+    """
+    result = {earthly: [] for earthly in EARTHLYS}
+    birth_year_heavenaly_index = HEAVENLYS.index(birth_year_heavenaly)
+    birth_year_earthly_index = EARTHLYS.index(birth_year_earthly)
+    birth_hour_index = EARTHLYS.index(birth_hour)
+    # 擎羊、陀羅 input: birth_year_heavenaly
+    rule = [
+        (3, 1), (4, 2), (6, 4), (7, 5), (6, 4),
+        (7, 5), (9, 7), (10, 8), (0, 10), (1, 11),
+    ]
+    result[EARTHLYS[rule[birth_year_heavenaly_index][0]]].append("擎羊")
+    result[EARTHLYS[rule[birth_year_heavenaly_index][1]]].append("陀羅")
+    # 火星、鈴星 input: birth_year_earthly, birth_hour
+    rule = [(2, 10), (3, 10), (1, 3), (9, 10)]
+    result[
+        EARTHLYS[rule[birth_year_earthly_index % 4][0] + birth_hour_index]
+    ].append("火星")
+    result[
+        EARTHLYS[rule[birth_year_earthly_index % 4][1] + birth_hour_index]
+    ].append("鈴星")
+    # 地劫、地空 input: birth_hour
+    # 地劫: 從 亥宮位開始順時針 11 + (子時=0)
+    # 地空: 從 亥宮位開始逆時針 11 - (子時=0)
+    result[EARTHLYS[(11 + birth_hour_index) % 12]].append("地劫")
+    result[EARTHLYS[(11 - birth_hour_index) % 12]].append("地空")
+    return result
+
+
+def stars_LuQuanKeJi(birth_year_heavenaly: str) -> dict:
+    """四化星
+    Ex: 出生 2023-03-01 丑時(農曆癸卯年二月初十 丑時)
+    input: "癸"
+    output: {'祿': '破軍', '權': '巨門', '科': '太陰', '忌': '貪狼'}
+
+    Args:
+        birth_year_heavenaly (str): 出生年(天干)
+
+    Returns:
+        dict: 四化對應的星曜
+    """
+    birth_year_heavenaly_index = HEAVENLYS.index(birth_year_heavenaly)
+    rule = [
+        ("廉貞", "破軍", "武曲", "太陽"), ("天機", "天梁", "紫微", "太陰"),
+        ("天同", "天機", "文昌", "廉貞"), ("太陰", "天同", "天機", "巨門"),
+        ("貪狼", "太陰", "太陽", "天機"), ("武曲", "貪狼", "天梁", "文曲"),
+        ("太陽", "武曲", "天府", "天同"), ("巨門", "太陽", "文曲", "文昌"),
+        ("天梁", "紫微", "天府", "武曲"), ("破軍", "巨門", "太陰", "貪狼"),
+    ]
+    LuQuanKeJi = rule[birth_year_heavenaly_index]
+    return dict(zip(("祿", "權", "科", "忌"), LuQuanKeJi))
